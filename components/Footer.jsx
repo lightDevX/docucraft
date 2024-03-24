45822ded5648063d77218148712f7eeeb15c3d64
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2024-03-24 13:30:04 +0600

    footer component create

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div>
-            <h2 className="mx-auto p-2 -m-2 text-center text-teal-700 font-bold bg-gradient-to-r from-indigo-300 via-red-200 to-teal-200">Developed By <strong><Link href="https://github.com/lightDevX">Hasan U Khan</Link></strong></h2>
+            <h2 className="mx-auto p-2 -m-2 text-center text-teal-700 font-bold bg-gradient-to-r from-indigo-300 via-red-200 to-teal-200">Developed By <strong><Link href="https://github.com/lightDevX"> Hasan U Khan </Link></strong></h2>
         </div >
     );
 };
