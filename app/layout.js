35f68a4b1e5867d7c52d97cadd5d859eba0fd15c
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2024-03-24 13:24:55 +0600

    footer component create

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Loading from "@/components/Loading";
 import { getDocuments } from "@/lib/docs";
@@ -25,7 +26,7 @@ export default function RootLayout({ children }) {
           <Suspense fallback={<Loading />}>
             <Header docs={allDocuments} />
             <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
-              <main className="flex-auto py-16">
+              <main className="flex-auto py-12">
                 <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
                   <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
                     <div className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100"></div>
@@ -33,6 +34,7 @@ export default function RootLayout({ children }) {
                 </div>
                 {children}
               </main>
+              <Footer />
             </div>
           </Suspense>
         </div>
